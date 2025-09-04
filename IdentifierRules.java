d284b65d35d6b32e11740a4ef1b330694ceaa7c2
Noe Noe <j24041682@student.newinti.edu.my>
2025-09-04 13:15:21 +0800

    Create IdentifierRules.java
    resolve errors

---
+++ IdentifierRules.java
@@ -0,0 +1,55 @@
+/**********************************************************************
+  
+  Program Title : Demonstrating Identifier Rules in Java
+  File Name: "IdentifierRules.java"
+  
+  Programmer : Noe Noe Htet
+  Date : 04 September 2025
+  
+ **********************************************************************/
+ 
+// Intentionally incorrect program to demonstrate invalid Java identifiers.
+
+//class switch{
+class SwitchStatus{
+	String SwitchStatus = "On";
+	
+}
+
+public class IdentifierRules {
+	
+    //public static void 1start() { 
+    public static void start() { 
+		System.out.println("Static Method Output");
+	}
+
+    public static void main(String[] args) {
+
+        //int 2ndPlace = 2;
+        int secondplace = 2;
+
+        //int first name = 10;
+        int first_name = 10;
+
+        //int price# = 99;
+        int price = 99;
+		//int user-name = 25;
+		int username = 25; 
+
+		//int class = 1;
+		int varClass = 1; 
+		//int double = 3;
+		int varDouble = 3;
+
+        int Score = 10;
+        //System.out.println(score);
+        System.out.println(Score);
+		
+		System.out.println("This program is executed after all errors are rectified.");
+
+
+        //1start();
+        start();
+    }
+
+}
