b40a49b894e9b23dd701e635b6cd847cd375ca8f
Noe Noe <j24041682@student.newinti.edu.my>
2025-09-07 21:48:13 +0800

    Create VariableScope.java

---
+++ VariableScope.java
@@ -0,0 +1,49 @@
+/**********************************************************************
+  File Name: "VariableScope.java"
+  
+  Programmer : NOE NOE
+ 
+ **********************************************************************/
+
+public class VariableScope {
+
+    // Static variable (class-level, shared by all objects)
+    static int staticVar = 100;
+
+    // Instance variable (object-level, each object has its own copy)
+    int instanceVar = 50;
+
+    public void printOutputMethod() {
+        // Local variable (method-level, exists only inside this method)
+        int localVar = 10;
+
+        System.out.println("Local Variable: " + localVar);
+
+        // Print Instance Variable and Static Variable
+        System.out.println("Instance Variable: " + instanceVar);
+        System.out.println("Static Variable: " + staticVar);
+    }
+
+    public static void main(String[] args) {
+        // Create Object 1
+        VariableScope obj1 = new VariableScope();
+        // Create Object 2
+        VariableScope obj2 = new VariableScope();
+
+        System.out.println("---- Object 1 ----");
+        obj1.printOutputMethod();
+
+        // Change instanceVar in obj1
+        obj1.instanceVar = 200;
+
+        // Change staticVar (affects all objects)
+        VariableScope.staticVar = 500;
+
+        System.out.println("\n---- Object 2 ----");
+        obj2.printOutputMethod();
+
+        System.out.println("\n---- Object 1 Again ----");
+        obj1.printOutputMethod();
+    }
+}
+
